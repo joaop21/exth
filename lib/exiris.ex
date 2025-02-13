572244144731cb42f8fae3c8e4f5b9313e42277b
João <jpsilva9898@gmail.com>
2025-02-13 00:36:06 +0000

    Initialize Repository (#1)
    This PR adds the files for the beginning of the project.

---
+++ lib/exiris.ex
@@ -0,0 +1,18 @@
+defmodule Exiris do
+  @moduledoc """
+  Documentation for `Exiris`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exiris.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exiris_test.exs
@@ -0,0 +1,8 @@
+defmodule ExirisTest do
+  use ExUnit.Case
+  doctest Exiris
+
+  test "greets the world" do
+    assert Exiris.hello() == :world
+  end
+end
