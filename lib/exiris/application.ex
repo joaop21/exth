b8ea8608c4dcbdc33fac2535ba376df691c3711b
joaop21 <jpsilva9898@gmail.com>
2025-02-16 23:13:24 +0000

    initialize a counter when starting the app

--- lib/exiris/application.ex
+++ lib/exiris/application.ex
@@ -3,8 +3,12 @@ defmodule Exiris.Application do
 
   use Application
 
+  alias Exiris.RequestCounter
+
   @impl true
   def start(_type, _args) do
+    RequestCounter.create!()
+
     children = []
     opts = [strategy: :one_for_one, name: Exiris.Supervisor]
     Supervisor.start_link(children, opts)
