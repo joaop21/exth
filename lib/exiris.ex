94e0bdff8ea639e5b7face49582d295274bf50bb
joaop21 <jpsilva9898@gmail.com>
2025-02-20 01:03:46 +0000

    improve exiris main module doc

--- lib/exiris.ex
+++ lib/exiris.ex
@@ -1,18 +1,28 @@
 defmodule Exiris do
   @moduledoc """
-  Documentation for `Exiris`.
+  Exiris is an Elixir library for interacting with EVM-compatible blockchain nodes.
+
+  ## Key Features
+
+  - Type-safe JSON-RPC client
+  - Multiple transport options (HTTP, WebSocket, IPC)
+  - Auto-generated functions for standard Ethereum methods
+  - Built-in request ID management
+  - Configurable providers
+
+  ## Quick Start
+
+      # Define your client
+      defmodule MyClient do
+        use Exiris.PublicClient,
+          transport_type: :http,
+          rpc_url: "https://mainnet.infura.io/v3/YOUR-PROJECT-ID"
+      end
+
+      # Make RPC calls
+      {:ok, block_number} = MyClient.eth_block_number()
+      {:ok, balance} = MyClient.eth_get_balance("0x123...", "latest")
+
+  See `Exiris.PublicClient` for detailed usage instructions.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Exiris.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/exiris_test.exs
+++ test/exiris_test.exs
@@ -2,7 +2,7 @@ defmodule ExirisTest do
   use ExUnit.Case
   doctest Exiris
 
-  test "greets the world" do
-    assert Exiris.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Exiris.hello() == :world
+  # end
 end
