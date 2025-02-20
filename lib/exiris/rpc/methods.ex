83a19885f4f074490c4ab0aa67927389b202b994
joaop21 <jpsilva9898@gmail.com>
2025-02-20 01:07:29 +0000

    add doc for rpc methods

--- lib/exiris/rpc/methods.ex
+++ lib/exiris/rpc/methods.ex
@@ -1,9 +1,17 @@
 defmodule Exiris.Rpc.Methods do
+  @moduledoc """
+  Defines the available Ethereum JSON-RPC methods and their parameters.
+  """
+
   @public_methods %{
     eth_blockNumber: [],
     eth_getBlockByNumber: [:block_number, :full_transactions],
     eth_getBlockByHash: [:block_hash, :full_transactions]
   }
 
+  @doc """
+  Returns a map of all supported public RPC methods and their parameters.
+  """
+  @spec public_methods() :: %{atom() => list(atom())}
   def public_methods, do: @public_methods
 end
