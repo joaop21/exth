d6c8dda69a5acd5b257bca1d116816e567d5114b
joaop21 <jpsilva9898@gmail.com>
2025-02-26 19:07:19 +0000

    add public methods to the rpc

--- lib/exiris/rpc/methods.ex
+++ lib/exiris/rpc/methods.ex
@@ -4,9 +4,48 @@ defmodule Exiris.Rpc.Methods do
   """
 
   @public_methods %{
+    web3_clientVersion: [],
+    web3_sha3: [:data],
+    net_version: [],
+    net_listening: [],
+    net_peerCount: [],
+    eth_protocolVersion: [],
+    eth_syncing: [],
+    eth_coinbase: [],
+    eth_chainId: [],
+    eth_mining: [],
+    eth_hashrate: [],
+    eth_gasPrice: [],
+    eth_accounts: [],
     eth_blockNumber: [],
+    eth_getBlockTransactionCountByHash: [:block_hash],
+    eth_getBlockTransactionCountByNumber: [:block_number],
+    eth_getUncleCountByBlockHash: [:block_hash],
+    eth_getUncleCountByBlockNumber: [:block_number],
+    eth_estimateGas: [:transaction],
+    eth_getBlockByHash: [:block_hash, :full_transactions],
     eth_getBlockByNumber: [:block_number, :full_transactions],
-    eth_getBlockByHash: [:block_hash, :full_transactions]
+    eth_getTransactionByHash: [:transaction_hash],
+    eth_getTransactionByBlockHashAndIndex: [:block_hash, :transaction_index],
+    eth_getTransactionByBlockNumberAndIndex: [:block_number, :transaction_index],
+    eth_getTransactionReceipt: [:transaction_hash],
+    eth_getUncleByBlockHashAndIndex: [:block_hash, :uncle_index],
+    eth_getUncleByBlockNumberAndIndex: [:block_number, :uncle_index],
+    eth_newFilter: [:filter],
+    eth_newBlockFilter: [],
+    eth_newPendingTransactionFilter: [],
+    eth_uninstallFilter: [:filter_id],
+    eth_getFilterChanges: [:filter_id],
+    eth_getFilterLogs: [:filter_id],
+    eth_getLogs: [:filter]
+  }
+
+  @default_block_number_public_methods %{
+    eth_getBalance: [:address],
+    eth_getStorageAt: [:address, :position],
+    eth_getTransactionCount: [:address],
+    eth_getCode: [:address],
+    eth_call: [:transaction]
   }
 
   @doc """
@@ -14,4 +53,13 @@ defmodule Exiris.Rpc.Methods do
   """
   @spec public_methods() :: %{atom() => list(atom())}
   def public_methods, do: @public_methods
+
+  @doc """
+  Returns a map of all supported public RPC methods with a default block number.
+  """
+  @spec public_methods_for_block_number() :: %{atom() => list(atom())}
+  def public_methods_for_block_number, do: @default_block_number_public_methods
+
+  def default_block_number_public_methods,
+    do: @default_block_number_public_methods
 end
