c9f8d56bbc8e4e179baf743ee5abb625caa0cc79
joaop21 <jpsilva9898@gmail.com>
2025-02-28 16:49:18 +0000

    http client should use encoder and decoder from opts

--- lib/exiris/transport/http.ex
+++ lib/exiris/transport/http.ex
@@ -2,7 +2,6 @@ defmodule Exiris.Transport.Http do
   @moduledoc """
   HTTP transport implementation for Exiris using Tesla.
   """
-  alias Exiris.Rpc
 
   @behaviour Exiris.Transport
 
@@ -10,14 +9,8 @@ defmodule Exiris.Transport.Http do
 
   @impl true
   def request(body, opts) do
-    with {:ok, response} <- do_post(client(opts), body) do
-      {:ok, Rpc.parse_response(response["id"], response["jsonrpc"], response["result"])}
-    else
-      {:error, reason} -> {:error, {:request_failed, reason}}
-    end
-  end
+    client = client(opts)
 
-  defp do_post(client, body) do
     case Tesla.post(client, "", body) do
       {:ok, %Tesla.Env{status: 200, body: response}} -> {:ok, response}
       {:ok, %Tesla.Env{status: status}} -> {:error, {:http_error, status}}
@@ -30,12 +23,15 @@ defmodule Exiris.Transport.Http do
     http_opts = opts[:http_opts] || []
     adapter = http_opts[:adapter] || @adapter
 
-    Tesla.client(
-      [
-        {Tesla.Middleware.BaseUrl, rpc_url},
-        Tesla.Middleware.JSON
-      ],
-      adapter
-    )
+    middleware = [{Tesla.Middleware.BaseUrl, rpc_url}] ++ build_json_middleware(opts)
+
+    Tesla.client(middleware, adapter)
+  end
+
+  defp build_json_middleware(opts) do
+    encoder = opts[:encoder]
+    decoder = opts[:decoder]
+
+    [{Tesla.Middleware.JSON, encode: encoder, decode: decoder}]
   end
 end
