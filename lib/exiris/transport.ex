c8d9f75b8dde36b0a604dffd30ce8f3e3c61c659
joaop21 <jpsilva9898@gmail.com>
2025-03-03 14:33:19 +0000

    update transport to be compliant with the behaviour

--- lib/exiris/transport.ex
+++ lib/exiris/transport.ex
@@ -5,31 +5,71 @@ defmodule Exiris.Transport do
 
   alias Exiris.Rpc.Request
   alias Exiris.Rpc.Response
+  alias __MODULE__.Http
 
-  @type type :: :http
+  @type type :: :custom | :http
+  @type encoder :: (Request.t() -> String.t())
+  @type decoder :: (String.t() -> Response.t())
 
-  @type http_opts :: [
-          adapter: Tesla.Client.adapter(),
-          headers: Keyword.t(),
-          timeout: non_neg_integer()
-        ]
-  @type opts :: [
+  @type t :: %__MODULE__{
+          decoder: encoder(),
+          encoder: decoder(),
+          module: __MODULE__.Behaviour.t() | nil,
+          opts: Http.opts() | map(),
           rpc_url: String.t(),
-          encoder: (Request.t() -> String.t()),
-          decoder: (String.t() -> Response.t()),
-          http_opts: http_opts()
-        ]
+          type: type()
+        }
 
-  @callback request(body :: Request.t(), opts :: opts()) :: {:ok, Response.t()} | {:error, any()}
+  defstruct [
+    :rpc_url,
+    :encoder,
+    :decoder,
+    :module,
+    :opts,
+    :type
+  ]
 
-  @spec get_by_type!(type()) :: module()
-  def get_by_type!(type) do
-    case fetch_by_type(type) do
-      {:ok, transport} -> transport
-      {:error, reason} -> raise ArgumentError, message: reason
-    end
+  ### 
+  ### Public Functions
+  ###
+
+  @behaviour __MODULE__.Behaviour
+
+  @impl true
+  def build_opts(opts) do
+    rpc_url = opts[:rpc_url] || raise ArgumentError, "missing required option :rpc_url"
+    encoder = opts[:encoder]
+    decoder = opts[:decoder]
+    type = opts[:transport_type]
+
+    module =
+      case type do
+        :http -> Exiris.Transport.Http
+        :custom -> opts[:module] || raise ArgumentError, "missing required option :module"
+        _ -> raise(ArgumentError, "invalid transport type: #{inspect(type)}")
+      end
+
+    # transport specific options
+    transport_opts = module.build_opts(opts[:opts] || [])
+
+    %__MODULE__{
+      rpc_url: rpc_url,
+      encoder: encoder,
+      decoder: decoder,
+      opts: transport_opts,
+      type: type,
+      module: module
+    }
   end
 
-  defp fetch_by_type(:http), do: {:ok, Exiris.Transport.Http}
-  defp fetch_by_type(_), do: {:error, :invalid_transport}
+  @impl true
+  def request(%__MODULE__{} = transport, body) do
+    transport.module.request(transport, body)
+  end
+
+  @spec new(type(), keyword()) :: __MODULE__.t()
+  def new(type, opts \\ []) do
+    opts = Keyword.merge(opts, transport_type: type)
+    build_opts(opts)
+  end
 end
