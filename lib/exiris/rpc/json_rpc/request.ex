ea8d03ab60bc6832055ed7c2e505a8cab6454596
joaop21 <jpsilva9898@gmail.com>
2025-03-17 01:07:30 +0000

    add function to serialize request

--- lib/exiris/rpc/json_rpc/request.ex
+++ lib/exiris/rpc/json_rpc/request.ex
@@ -1,5 +1,7 @@
 defmodule Exiris.Rpc.JsonRpc.Request do
+  alias Exiris.Rpc.Encoding
   alias Exiris.Rpc.JsonRpc
+  alias Exiris.Rpc.JsonRpc.SerializedRequest
 
   @type t :: %__MODULE__{
           id: JsonRpc.id(),
@@ -21,5 +23,15 @@ defmodule Exiris.Rpc.JsonRpc.Request do
       params: params,
       id: id
     }
+  end
+
+  def serialize(%__MODULE__{} = request) do
+    with {:ok, request} <- Encoding.encode_request(request) do
+      %SerializedRequest{
+        id: request.id,
+        method: request.method,
+        request: request
+      }
+    end
   end
 end
