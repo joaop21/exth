b4a80f3413050c5f14f32e5e0de41b8346525daa
joaop21 <jpsilva9898@gmail.com>
2025-03-17 16:12:01 +0000

    remove serialized request

--- lib/exiris/rpc/json_rpc/request.ex
+++ lib/exiris/rpc/json_rpc/request.ex
@@ -1,7 +1,5 @@
 defmodule Exiris.Rpc.JsonRpc.Request do
-  alias Exiris.Rpc.Encoding
   alias Exiris.Rpc.JsonRpc
-  alias Exiris.Rpc.JsonRpc.SerializedRequest
 
   @type t :: %__MODULE__{
           id: JsonRpc.id(),
@@ -23,15 +21,5 @@ defmodule Exiris.Rpc.JsonRpc.Request do
       params: params,
       id: id
     }
-  end
-
-  def serialize(%__MODULE__{} = request) do
-    with {:ok, request} <- Encoding.encode_request(request) do
-      %SerializedRequest{
-        id: request.id,
-        method: request.method,
-        request: request
-      }
-    end
   end
 end
