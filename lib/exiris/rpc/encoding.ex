3dcc1fc86792fd437efd561b6f80119624998d0a
joaop21 <jpsilva9898@gmail.com>
2025-03-19 17:13:23 +0000

    improve encoding to deal with batch requests

--- lib/exiris/rpc/encoding.ex
+++ lib/exiris/rpc/encoding.ex
@@ -6,25 +6,35 @@ defmodule Exiris.Rpc.Encoding do
   alias Exiris.Rpc.Response
   alias Exiris.Rpc.Request
 
-  @spec encode_request(Request.t()) :: {:ok, String.t()} | {:error, Exception.t()}
+  @spec encode_request(Request.t() | [Request.t()]) :: {:ok, String.t()} | {:error, Exception.t()}
   def encode_request(%Request{} = request) do
     request
-    |> Map.from_struct()
+    |> do_encode_request()
     |> Jason.encode()
   end
 
+  def encode_request(requests) when is_list(requests) do
+    requests
+    |> Enum.map(&do_encode_request/1)
+    |> Jason.encode()
+  end
+
+  defp do_encode_request(%Request{} = request), do: Map.from_struct(request)
+
   @spec decode_response(String.t()) ::
-          {:ok, Response.t()} | {:error, Exception.t()}
+          {:ok, Response.t() | [Response.t()]} | {:error, Exception.t()}
   def decode_response(json) do
-    case Jason.decode(json) do
-      {:ok, %{"id" => id, "result" => result}} ->
-        {:ok, Response.success(id, result)}
-
-      {:ok, %{"id" => id, "error" => error}} ->
-        {:ok, Response.error(id, error["code"], error["message"])}
-
-      error ->
-        {:error, error}
+    with {:ok, response} <- Jason.decode(json) do
+      {:ok, do_decode_response(response)}
     end
+  end
+
+  defp do_decode_response(%{"id" => id, "result" => result}), do: Response.success(id, result)
+
+  defp do_decode_response(%{"id" => id, "error" => error}),
+    do: Response.error(id, error["code"], error["message"])
+
+  defp do_decode_response(response) when is_list(response) do
+    Enum.map(response, &do_decode_response/1)
   end
 end
