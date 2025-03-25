86b077bcfaa8710438675b3b0f245e7e972f6e43
joaop21 <jpsilva9898@gmail.com>
2025-03-25 11:46:06 +0000

    improve request and tests

--- test/exth/rpc/request_test.exs
+++ test/exth/rpc/request_test.exs
@@ -30,30 +30,76 @@ defmodule Exth.Rpc.RequestTest do
       assert request.id == id
     end
 
-    test "accepts different id types" do
-      test_cases = [
-        {1, "eth_getBalance", ["0x123"]},
-        {"1", "eth_blockNumber", []},
-        {"abc-123", "eth_chainId", []}
+    test "accepts atom as method" do
+      request = Request.new(:eth_getBalance, [], 1)
+      assert request.method == :eth_getBalance
+    end
+
+    test "raises when method is invalid" do
+      invalid_methods = [
+        nil,
+        123,
+        %{},
+        [],
+        true,
+        "",
+        "   "
       ]
 
-      for {id, method, params} <- test_cases do
-        request = Request.new(method, params, id)
-        assert %Request{id: ^id} = request
+      for method <- invalid_methods do
+        assert_raise ArgumentError, ~r/invalid method/i, fn ->
+          Request.new(method, [], 1)
+        end
       end
     end
 
-    test "accepts different param types" do
-      test_cases = [
-        {["0x123", "latest"], "eth_getBalance"},
-        {[1, 2, 3], "test_method"},
-        {[%{key: "value"}], "test_complex"},
-        {[], "eth_blockNumber"}
+    test "raises when id is not a positive integer" do
+      invalid_ids = [
+        nil,
+        0,
+        -1,
+        "1",
+        1.5,
+        true,
+        [],
+        %{}
       ]
 
-      for {params, method} <- test_cases do
-        request = Request.new(method, params, 1)
-        assert %Request{params: ^params} = request
+      for id <- invalid_ids do
+        assert_raise ArgumentError, ~r/invalid id/i, fn ->
+          Request.new("eth_call", [], id)
+        end
+      end
+    end
+
+    test "raises when params is not a list" do
+      invalid_params = [
+        nil,
+        "string",
+        123,
+        true,
+        %{},
+        MapSet.new()
+      ]
+
+      for params <- invalid_params do
+        assert_raise ArgumentError, ~r/invalid params/i, fn ->
+          Request.new("eth_call", params, 1)
+        end
+      end
+    end
+
+    test "accepts various types within params list" do
+      valid_params = [
+        [],
+        [1, "string", true, %{}, [1, 2, 3]],
+        ["0x123", "latest"],
+        [%{key: "value"}]
+      ]
+
+      for params <- valid_params do
+        request = Request.new("eth_call", params, 1)
+        assert request.params == params
       end
     end
   end
