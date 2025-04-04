6713429d8708105ae4a4df0fcb3214f59087ec4b
joaop21 <jpsilva9898@gmail.com>
2025-04-04 12:02:57 +0100

    improve multichain example

--- examples/multichain.exs
+++ examples/multichain.exs
@@ -1,5 +1,8 @@
 Mix.install([{:exth, path: "../"}, :mint])
 
+# Be cautious when running this example. The RPC URLs are public and may be
+# rate-limited or unstable.
+
 defmodule MyProvider do
   defmodule Eth do
     use Exth.Provider,
@@ -18,16 +21,18 @@ require Logger
 
 vitalik_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
 
-Enum.each([MyProvider.Eth, MyProvider.Polygon], fn provider -> 
+Enum.each([MyProvider.Eth, MyProvider.Polygon], fn provider ->
   {:ok, block_number} = provider.block_number()
   {:ok, balance} = provider.get_balance(vitalik_address, block_number)
   Logger.info("#{provider}: block_number: #{block_number}")
   Logger.info("#{provider}: get_balance: #{balance}")
 end)
 
-
 # Using RPC client directly
 client = MyProvider.Eth.get_client()
-request = Exth.Rpc.request(client, "eth_blockNumber", [])
-{:ok, response} = Exth.Rpc.send(client, request)
+
+{:ok, response} =
+  Exth.Rpc.request("eth_blockNumber", [])
+  |> Exth.Rpc.send(client)
+
 Logger.info("RPC: eth_blockNumber: #{inspect(response)}")
