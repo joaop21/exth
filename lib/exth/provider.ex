0fcf5ef997a8cf62b6c661b352c3340a8752c677
joaop21 <jpsilva9898@gmail.com>
2025-04-04 17:30:52 +0100

    split provide generation into different macros

--- lib/exth/provider.ex
+++ lib/exth/provider.ex
@@ -79,6 +79,11 @@ defmodule Exth.Provider do
   See `Exth.Provider.Methods` for a complete list of available RPC methods.
   """
 
+  alias Exth.Provider.ClientCache
+  alias Exth.Provider.Methods
+  alias Exth.Rpc
+  alias Exth.Rpc.Response
+
   @required_opts [:transport_type, :rpc_url]
 
   @doc """
@@ -158,61 +163,74 @@ defmodule Exth.Provider do
   """
   defmacro generate_provider(opts) do
     quote bind_quoted: [opts: opts] do
-      alias Exth.Provider.ClientCache
-      alias Exth.Provider.Methods
-      alias Exth.Rpc
-      alias Exth.Rpc.Response
-
       @type rpc_response :: {:ok, term()} | {:error, term()}
 
       @default_block_tag "latest"
       @provider_opts opts
 
       for {method_name, {rpc_method, param_types, accepts_block}} <- Methods.methods() do
-        param_vars = Enum.map(param_types, &Macro.var(&1, nil))
-
-        {function_params, request_params} =
-          if accepts_block do
-            block_param = Macro.var(:block_tag, nil)
-            function_params = param_vars ++ [{:\\, [], [block_param, @default_block_tag]}]
-            request_params = param_vars ++ [block_param]
-            {function_params, request_params}
-          else
-            {param_vars, param_vars}
-          end
-
-        param_docs = Enum.map_join(param_types, "\n* ", &"#{&1}")
-
-        param_docs =
-          if accepts_block,
-            do:
-              param_docs <>
-                "\n* block_tag - Block number or tag (default: \"#{@default_block_tag}\")",
-            else: param_docs
-
-        @doc """
-        Executes the #{rpc_method} JSON-RPC method.
-
-        ## Parameters
-        * #{param_docs}
-
-        ## Returns
-          * `{:ok, response}` - Successful request with decoded response
-          * `{:error, reason}` - Request failed with error details
-        """
-        @spec unquote(method_name)(
-                unquote_splicing(param_types |> Enum.map(fn _ -> quote do: term() end))
-              ) :: rpc_response()
-        def unquote(method_name)(unquote_splicing(function_params)) do
-          client = get_client()
-
-          unquote(rpc_method)
-          |> Rpc.request([unquote_splicing(request_params)])
-          |> Rpc.send(client)
-          |> handle_response()
-        end
+        Exth.Provider.generate_rpc_method(method_name, rpc_method, param_types, accepts_block)
       end
 
+      Exth.Provider.generate_get_client()
+      Exth.Provider.generate_handle_response()
+    end
+  end
+
+  defmacro generate_rpc_method(method_name, rpc_method, param_types, accepts_block) do
+    quote bind_quoted: [
+            method_name: method_name,
+            rpc_method: rpc_method,
+            param_types: param_types,
+            accepts_block: accepts_block
+          ] do
+      param_vars = Enum.map(param_types, &Macro.var(&1, nil))
+
+      {function_params, request_params} =
+        if accepts_block do
+          block_param = Macro.var(:block_tag, nil)
+          function_params = param_vars ++ [{:\\, [], [block_param, @default_block_tag]}]
+          request_params = param_vars ++ [block_param]
+          {function_params, request_params}
+        else
+          {param_vars, param_vars}
+        end
+
+      param_docs = Enum.map_join(param_types, "\n* ", &"#{&1}")
+
+      param_docs =
+        if accepts_block,
+          do:
+            param_docs <>
+              "\n* block_tag - Block number or tag (default: \"#{@default_block_tag}\")",
+          else: param_docs
+
+      @doc """
+      Executes the #{rpc_method} JSON-RPC method.
+
+      ## Parameters
+      * #{param_docs}
+
+      ## Returns
+        * `{:ok, response}` - Successful request with decoded response
+        * `{:error, reason}` - Request failed with error details
+      """
+      @spec unquote(method_name)(
+              unquote_splicing(param_types |> Enum.map(fn _ -> quote do: term() end))
+            ) :: rpc_response()
+      def unquote(method_name)(unquote_splicing(function_params)) do
+        client = get_client()
+
+        unquote(rpc_method)
+        |> Rpc.request([unquote_splicing(request_params)])
+        |> Rpc.send(client)
+        |> handle_response()
+      end
+    end
+  end
+
+  defmacro generate_get_client do
+    quote do
       @doc """
       Retrieves or creates a new RPC client instance.
 
@@ -257,7 +275,11 @@ defmodule Exth.Provider do
             ClientCache.create_client(transport_type, rpc_url, client)
         end
       end
+    end
+  end
 
+  defmacro generate_handle_response do
+    quote do
       defp handle_response({:ok, %Response.Success{} = response}), do: {:ok, response.result}
       defp handle_response({:ok, %Response.Error{} = response}), do: {:error, response.error}
       defp handle_response({:error, reason} = response) when is_binary(reason), do: response
