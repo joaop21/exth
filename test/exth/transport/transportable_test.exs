642cbab5791b669c11a6c8951f1aa3c566c6f841
joaop21 <jpsilva9898@gmail.com>
2025-05-18 11:18:24 +0100

    improve transportable test

--- test/exth/transport/transportable_test.exs
+++ test/exth/transport/transportable_test.exs
@@ -13,14 +13,14 @@ defmodule Exth.Transport.TransportableTest do
   describe "protocol requirements" do
     test "raises Protocol.UndefinedError when protocol is not implemented" do
       transport = %InvalidTransport{}
-      request = Request.new("eth_blockNumber", [], 1)
+      encoded_request = Request.new("eth_blockNumber", [], 1) |> Request.serialize()
 
       assert_raise Protocol.UndefinedError, @error_message, fn ->
         Transportable.new(transport, [])
       end
 
       assert_raise Protocol.UndefinedError, @error_message, fn ->
-        Transportable.call(transport, request)
+        Transportable.call(transport, encoded_request)
       end
     end
   end
