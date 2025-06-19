80f6b671ab99d5a451fc723d66de376af79e718e
joaop21 <jpsilva9898@gmail.com>
2025-06-19 12:04:03 +0100

    add transport registry

--- lib/exth/transport/supervisor.ex
+++ lib/exth/transport/supervisor.ex
@@ -3,12 +3,17 @@ defmodule Exth.Transport.Supervisor do
 
   use Supervisor
 
+  alias Exth.Transport
+
   def start_link(opts) do
     Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   def init(_opts) do
-    children = []
+    children = [
+      Transport.Registry
+    ]
+
     Supervisor.init(children, strategy: :one_for_one)
   end
 end
