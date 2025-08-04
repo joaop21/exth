bf932c18b468b58c43f8883da8dcc12ade87fcc6
joaop21 <jpsilva9898@gmail.com>
2025-08-04 23:09:29 +0100

    update helpers

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,7 @@ Code.require_file("support/test_helpers.exs", __DIR__)
 
 Mimic.copy(Exth.Rpc.MessageHandler)
 Mimic.copy(Exth.Transport)
+Mimic.copy(Exth.Transport.Ipc.ConnectionPool)
 Mimic.copy(Exth.Transport.Ipc.Socket)
 Mimic.copy(Exth.Transport.Websocket)
 Mimic.copy(Exth.Transport.Websocket.DynamicSupervisor)
